d36cc635a18ce619d04bb85a8567c50cfb89e41f
Your Name <782084625@qq.com>
2018-11-16 18:19:57 +0800

    简单运用组件，并打包

---
+++ src/App.vue
@@ -0,0 +1,296 @@
+<template>
+  <div id="app">
+    <div class="head">
+      <div class="banxin">
+        <div class="fl-l">
+          <a href="" class="big">Bootstrap中文网</a>
+          <a href="">Bootstrap2中文文档</a>
+          <a href="">Bootstrap3中文文档</a>
+          <a href="">Bootstrap4中文文档</a>
+          <a href="">Less 教程</a>
+          <a href="">jQuery API</a>
+          <a href="">网站实例<sup class="ab"></sup></a>
+        </div>
+        <div class="fl-r">
+          <a href="">关于</a>
+        </div>
+      </div>
+    </div>
+    <div class="banner">
+      <h1>Bootstrap</h1>
+      <h2>简洁、直观、强悍的前端开发框架，让web开发更迅速、简单。</h2>
+      <button><a href="">Bootstrap3中文文档(v3.3.7)</a></button>
+      <p><a href="">Bootstrap2中文文档(v2.3.2)</a></p>
+    </div>
+    <div class="banner-bottom">
+      <span>
+        <a href="" class="el-icon-edit">Bootstrap问答社区</a>
+        <i></i>
+        <a href="" class="el-icon-share red"> 新浪微博：@Bootstrap中文网</a>
+      </span>
+    </div>
+    <div class="nav">
+      <h1>Bootstrap相关优质项目推荐</h1>
+      <p>这些项目或者是对Bootstrap进行了有益的补充，或者是基于Bootstrap开发的</p>
+    </div>
+    <div class="box">
+      <div class="banxin">
+        <div class="item">
+          <img src="./assets/expo.png" alt="">
+          <h2>优站精选</h2>
+          <p class="p1">Bootstrap 网站实例</p>
+          <p>Bootstrap 优站精选频道收集了众多基于 Bootstrap 构建、设计精美的、有创意的网站。</p>
+        </div>
+         <div class="item">
+          <img src="./assets/expo.png" alt="">
+          <h2>优站精选</h2>
+          <p class="p1">Bootstrap 网站实例</p>
+          <p>Bootstrap 优站精选频道收集了众多基于 Bootstrap 构建、设计精美的、有创意的网站。</p>
+        </div>
+         <div class="item">
+          <img src="./assets/expo.png" alt="">
+          <h2>优站精选</h2>
+          <p class="p1">Bootstrap 网站实例</p>
+          <p>Bootstrap 优站精选频道收集了众多基于 Bootstrap 构建、设计精美的、有创意的网站。</p>
+        </div>
+         <div class="item">
+          <img src="./assets/expo.png" alt="">
+          <h2>优站精选</h2>
+          <p class="p1">Bootstrap 网站实例</p>
+          <p>Bootstrap 优站精选频道收集了众多基于 Bootstrap 构建、设计精美的、有创意的网站。</p>
+        </div>
+         <div class="item">
+          <img src="./assets/expo.png" alt="">
+          <h2>优站精选</h2>
+          <p class="p1">Bootstrap 网站实例</p>
+          <p>Bootstrap 优站精选频道收集了众多基于 Bootstrap 构建、设计精美的、有创意的网站。</p>
+        </div>
+         <div class="item">
+          <img src="./assets/expo.png" alt="">
+          <h2>优站精选</h2>
+          <p class="p1">Bootstrap 网站实例</p>
+          <p>Bootstrap 优站精选频道收集了众多基于 Bootstrap 构建、设计精美的、有创意的网站。</p>
+        </div>
+        <div class="item">
+          <img src="./assets/expo.png" alt="">
+          <h2>优站精选</h2>
+          <p class="p1">Bootstrap 网站实例</p>
+          <p>Bootstrap 优站精选频道收集了众多基于 Bootstrap 构建、设计精美的、有创意的网站。</p>
+        </div>
+        <div class="item">
+          <img src="./assets/expo.png" alt="">
+          <h2>优站精选</h2>
+          <p class="p1">Bootstrap 网站实例</p>
+          <p>Bootstrap 优站精选频道收集了众多基于 Bootstrap 构建、设计精美的、有创意的网站。</p>
+        </div>
+        <div class="item">
+          <img src="./assets/expo.png" alt="">
+          <h2>优站精选</h2>
+          <p class="p1">Bootstrap 网站实例</p>
+          <p>Bootstrap 优站精选频道收集了众多基于 Bootstrap 构建、设计精美的、有创意的网站。</p>
+        </div>
+        <div class="item">
+          <img src="./assets/expo.png" alt="">
+          <h2>优站精选</h2>
+          <p class="p1">Bootstrap 网站实例</p>
+          <p>Bootstrap 优站精选频道收集了众多基于 Bootstrap 构建、设计精美的、有创意的网站。</p>
+        </div>
+        <div class="item">
+          <img src="./assets/expo.png" alt="">
+          <h2>优站精选</h2>
+          <p class="p1">Bootstrap 网站实例</p>
+          <p>Bootstrap 优站精选频道收集了众多基于 Bootstrap 构建、设计精美的、有创意的网站。</p>
+        </div>
+        <div class="item">
+          <img src="./assets/expo.png" alt="">
+          <h2>优站精选</h2>
+          <p class="p1">Bootstrap 网站实例</p>
+          <p>Bootstrap 优站精选频道收集了众多基于 Bootstrap 构建、设计精美的、有创意的网站。</p>
+        </div>
+      </div>
+    </div>
+    <footer0></footer0>
+  </div>
+</template>
+
+<script>
+import Footer from './components/footer'
+export default {
+  name: 'App'
+  ,
+  data(){
+      return{}
+  },
+  components:{
+    "footer0":Footer
+  }
+}
+</script>
+
+<style scoped>
+#app {
+  width: 100%;
+  position:absolute;
+  right:0;
+  left: 0;
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+}
+*{
+  margin: 0;
+  padding: 0;
+}
+li{
+  list-style: none;
+}
+a{
+  color: #fff;
+  text-decoration: none;
+}
+html body .big{
+  font-size:16px;
+}
+.banxin{
+  width: 1170px;
+  margin: 0 auto;
+  overflow: hidden;
+}
+.fl-l{
+  float:left;
+}
+.fl-r{
+  float:right;
+}
+.re{
+  position: relative;
+}
+.ab{
+  position: absolute;
+}
+.head{
+  position: fixed;
+  top:0;
+  left: 0;
+  right: 0;
+  border-bottom:1px solid #000; 
+  height: 42px;
+  line-height: 42px;
+  background-color: #222;
+  border-color: #080808;
+}
+.head a{
+  font-size: 14px;
+  color: #9d9d9d;
+  float:left;
+  display:inline-block; 
+}
+.head a:hover{
+   color:#fff;
+}
+.head .fl-l a+a{
+  margin-left:30px
+}
+.head sup{
+  width: 8px;
+  height: 8px;
+  border-radius: 50%;
+  background-color: #d9534f;
+  top:9px;
+}
+.banner{
+  padding: 90px 0 110px;
+  color:#fff;
+  text-align: center;
+  background:linear-gradient(45deg,#020031 0,#6d3353 100%);
+  background-image: linear-gradient(45deg, rgb(2, 0, 49) 0px, rgb(109, 51, 83) 100%);
+}
+.banner h1{
+  font-size: 100px;
+  font-weight: 700;
+}
+.banner h2{
+  font-size: 30px;
+  font-weight:normal;
+}
+.banner button{
+  box-shadow: inset 0 -4px 0 #2a6496; 
+  margin:20px 0;
+  font-size: 18px;
+  border-radius: 6px;
+  padding: 13px 35px 17px;
+  background-color: #337ab7;
+  border:none;
+}
+.banner-bottom{
+  height: 54px;
+  line-height: 54px;
+  text-align: center;
+  background-color: #f5f5f5;
+  border-top: 1px solid #fff;
+  border-bottom: 1px solid #ddd;
+}
+.banner-bottom a{
+  color: #000;
+  content: "|";
+
+}
+.banner-bottom a:hover{
+  text-decoration: underline;
+}
+.banner-bottom .red:hover
+{
+  color: #d9534f;
+}
+.banner-bottom i{
+  display:inline-block;
+  width:1px;
+  height:15px;
+  margin:0 10px;
+  position: relative;
+  top:3px;
+  background-color:#ccc;
+}
+.nav{
+  width: 684px;
+  border-bottom:1px solid #eee; 
+  margin:50px auto 40px auto; 
+}
+.nav h1{
+  font-size:42px;
+  font-weight: normal;
+  margin-bottom: 10px;
+}
+.nav p{
+  margin: 0 0 20px;
+}
+.box .item{
+  margin-bottom:40px; 
+  height: 336px;
+  margin-right:3.33%;
+  width: 22.5%;
+  box-sizing: border-box;
+  padding: 4px;
+  border:1px solid #ddd; 
+  float: left;
+}
+.box .item img{
+  width: 100%;
+}
+.box .item h2{
+  margin-top:20px;
+  color: red;
+  font-weight:normal;
+}
+.box .item .p1{
+  margin:7px 0 10px 0; 
+}
+.box .item p{
+  color: #777;
+}
+.box .item:nth-child(4n){
+ margin-right:0;
+}
+</style>
---
+++ src/components/footer.vue
@@ -0,0 +1,116 @@
+<template>
+  <div id="footer">
+    <div class="fotter-bottom">
+		<div class="left">
+			<div style="width: 100%;height: 30px;">
+				<img src="../assets/logo.png" alt="">
+			</div>
+			<p>我们一直致力于为广大开发者提供更多的优质技术文档和辅助开发工具！</p>
+		</div>
+		<div class="right">
+			<table>
+				<tr>
+					<th>关于</th>
+					<th>联系方式</th>
+					<th>旗下网站</th>
+					<th>赞助商</th>
+				</tr>
+				<tr>
+					<td><a href="">关于我们</a></td>
+					<td><a href="">新浪微博</a></td>
+					<td><a href="">Laravel中文网</a></td>
+					<td><a href="">猫云</a></td>
+				</tr>
+				<tr>
+					<td><a href="">广告合作</a></td>
+					<td><a href="">电子邮件</a></td>
+					<td><a href="">Ghost中国</a></td>
+					<td><a href="">京东云</a></td>
+				</tr>
+				<tr>
+					<td><a href="">友情链接</a></td>
+					<td><a href=""></a></td>
+					<td><a href="">BootCDN</a></td>
+					<td><a href="">又拍云</a></td>
+				</tr>
+				<tr>
+					<td><a href="">招聘</a></td>
+					<td><a href=""></a></td>
+					<td><a href="">Packagist中国镜像</a></td>
+					<td><a href=""></a></td>
+				</tr>
+				<tr>
+					<td><a href=""></a></td>
+					<td><a href=""></a></td>
+					<td><a href="">燃腾教育</a></td>
+					<td><a href=""></a></td>
+				</tr>
+			</table>
+		</div>
+    </div>
+    <p class="p2">京ICP备11008151号<span></span>京公网安备11010802014853</p>
+  </div>
+</template>
+
+<script>
+
+export default {
+  name: 'Footer'
+}
+</script>
+
+<style scoped>
+#footer{
+	border-top:1px solid #ddd;
+	margin-bottom:50px; 
+}
+.fotter-bottom{
+	padding-top:40px; 
+	color: #777;
+	width:1170px;
+	overflow:hidden;
+	margin: 0 auto;
+	font-size:12px; 
+ 	border-bottom:1px solid #ddd;
+}
+.left{
+	float: left;
+}
+.right{
+	float: right;
+}
+table a{
+	color: #777;
+	text-decoration: none;
+}
+table a:hover{
+	text-decoration:underline;
+}
+table{
+	text-align: left;
+}
+table th{
+	font-size: 16px;
+	width:127px;
+	font-weight: normal;
+	color: #563d7c;
+	padding-bottom:8px; 
+}
+table td{
+	font-size: 14px;
+}
+.p2{
+	font-size: 14px;
+	color: #777;
+}
+.p2 span{
+	width: 1px;
+	display: inline-block;
+	height:16px;
+	background-color: #ddd;
+	margin:0 8px; 
+}
+img{
+	float: left;
+}
+</style>
